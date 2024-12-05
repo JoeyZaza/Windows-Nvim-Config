50da2845ad7779d8ea73d9ba8e61617d4252f132
joeyzaza <joeyzaza324@gmail.com>
2024-12-04 21:50:50 -0500

    Added highlight on yank

--- init.lua
+++ init.lua
@@ -1,10 +1,18 @@
 --print("HELLO FROM init.lua")
 require('keymaps')
-require('lazy.lazy')
+require('config.lazy')
 require('lazy').setup('plugins')
+
 require('options')
 
 vim.cmd("colorscheme tokyonight-storm")
 vim.api.nvim_set_hl(0, "Normal", {bg = "none"}) -- this sets the background to transparent I guess
 --require('mason_config')
 
+vim.api.nvim_create_autocmd('TextYankPost', {
+	desc = 'Highlight when yanking text',
+	group = vim.api.nvim_create_augroup('highlight-yank', {clear = true}),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,20 +1,10 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>ex", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>qq", ":q!<CR>")
---vim.keymap.set("n", "<Leader>wq", ":wq<CR>", {noremap = true}) --old way, maybe the better way? idk...
---vim.keymap.del("n", "<Leader>wq")
 vim.keymap.set("n", "<Leader>wq", vim.cmd.wq)
---vim.keymap.set("n", "<Leader>ww", ":w<CR>", {noremap = true}) --old way, maybe the better way? idk...
---vim.keymap.del("n", "<Leader>ww")
 vim.keymap.set("n", "<Leader>ww", vim.cmd.write)
---vim.keymap.set("n", "<Leader>w", ":wq<CR>", {noremap = true})(
---vim.api.nvim_del_keymap("n", "<Leader>tc", ":tabclose<CR>",{noremap = true})
 vim.keymap.set("n", "<leader>tn", ":tabnew<CR>")
 vim.keymap.set("n", "<leader>te", ":tabc<CR>")
 vim.keymap.set("n", "<leader>to", ":tabo<CR>")
 vim.keymap.set("n", "<a-l>", ":tabn<CR>")
 vim.keymap.set("n", "<a-h>", ":tabp<CR>")
---vim.keymap.set("n", "<leader><leader>x", function() 
---	vim.cmd.write
---	":so"
---end)
